65d77080cf8d87ac203ad01933337718a8be8413
klauson <zacc.ledford@gmail.com>
2015-08-09 06:17:16 -0400

    ready to begin

--- jonstickleytrio/inc/footer.php
+++ jonstickleytrio/inc/footer.php
@@ -3,6 +3,7 @@
 
 
   <script src="assets/js/smoothState.js" type="text/javascript" async="true" defer="defer"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <script src="assets/js/materialize.js" type="text/javascript"></script>
   <script src="assets/js/main.js" type="text/javascript" async="true" defer="defer"></script>
   <script src="assets/player/audioplayerengine/amazingaudioplayer.js"></script>
--- jonstickleytrio/inc/header.php
+++ jonstickleytrio/inc/header.php
@@ -8,15 +8,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Use title if it's in the page YAML frontmatter -->
   <title>
-    Home - jQuery.smoothState.js
+    Jon Stickley Trio
   </title>
-  <meta name="description" content="smoothState.js lets you add transitions to eliminate the hard cuts and white flashes of page loads that deface the beauty of the user experience.">
-
+  <meta name="description" content="Jon Stickley Trio Band Website">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link href="assets/css/animate.css" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" href="assets/css/materialize.css">
-  <link href="assets/css/all.css" rel="stylesheet" type="text/css" />
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="assets/player/audioplayerengine/initaudioplayer-1.css">
+
+  <link href="assets/sass/main.css" rel="stylesheet" type="text/css" />
+
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
 
 </head>
 
@@ -24,17 +27,17 @@
 
 <nav>
   <div class="nav-wrapper">
-    <a href="#!" class="brand-logo"></a>
+<!--     <a href="#!" class="brand-logo"></a>
     <ul class="left hide-on-med-and-down">
       <li><a href="getting-started.php">Getting Started</a></li>
       <li><a href="typical-implementation.php">Typical</a></li>
       <li><a href="advanced-implementation.php">Advanced</a></li>
-    </ul>
+    </ul> -->
     <ul id="slide-out" class="side-nav">
       <li><a href="getting-started.php">Getting Started</a></li>
       <li><a href="typical-implementation.php">Typical</a></li>
       <li><a href="advanced-implementation.php">Advanced</a></li>
     </ul>
-    <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="fa fa-bars fa-3x"></i></a>
+    <a href="#" data-activates="slide-out" class="button-collapse"><i class="fa fa-bars fa-3x"></i></a>
   </div>
 </nav>
